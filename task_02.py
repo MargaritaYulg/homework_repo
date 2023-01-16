7d57852bd073568d21aed4a9f483f4037be8029e
MargaritaYulg <122794667+MargaritaYulg@users.noreply.github.com>
2023-01-16 16:57:27 +0400

    Добавла task_02.py с решением

---
+++ task_02.py
@@ -0,0 +1,6 @@
+#Задача№2
+#https://www.codewars.com/kata/57ee4a67108d3fd9eb0000e7
+
+geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+def goose_filter(birds):
+    return [bird for bird in birds if bird not in geese]
